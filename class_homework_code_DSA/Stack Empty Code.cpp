84a9366f56020ff3c33e3b3b21875c8710d36776
Code369J08A <199352386+J08A369@users.noreply.github.com>
2026-05-02 19:41:06 +0600

    Add files via upload

---
+++ class_homework_code_DSA/Stack Empty Code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+
+    stack<int> st;
+
+    if(st.empty()) {
+        cout << "Stack is empty";
+    }
+    else {
+        cout << "Stack is not empty";
+    }
+
+    return 0;
+}
